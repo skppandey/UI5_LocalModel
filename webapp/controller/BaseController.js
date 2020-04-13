8d016b38decd462a245119015d261df474f0c714
Shubham Pandey <shubhamkp026@gmail.com>
2020-04-13 15:23:32 +0000

    commit

---
+++ webapp/controller/BaseController.js
@@ -0,0 +1,53 @@
+sap.ui.define([
+  "sap/ui/core/mvc/Controller",
+  "sap/ui/core/UIComponent"
+], function (Controller, UIComponent) {
+  "use strict";
+ 
+  return Controller.extend("zmodel.zmodel.controller.BaseController", {
+ 
+    // some basic functionalities
+ 
+    // just this.getRouter() ...
+
+    getRouter: function () {
+     
+      // ... instead of
+      return UIComponent.getRouterFor(this);
+ 
+    },
+ 
+    // just this.getModel() ...
+    getModel: function (sName) {
+   
+      // ... instead of
+      //return this.getView().getModel(sName);
+      return this.getOwnerComponent().getModel(sName);
+ 
+    },
+ 
+    // just this.setModel() ...
+    setModel: function (oModel, sName) {
+      // ... instead of
+      return this.getView().setModel(oModel, sName);
+ 
+    },
+ 
+    // just this.getResoureBundle() ... 
+    getResourceBundle: function () {
+ 
+      // ... instead of
+      return this.getOwnerComponent().getModel("i18n").getResourceBundle();
+ 
+    },
+ 
+    // calculate something
+    randomCalculations: function(fValue1, fValue2) {
+ 
+      return (fValue1 + fValue2);
+ 
+    }
+ 
+  });
+ 
+});
--- webapp/controller/View1.controller.js
+++ webapp/controller/View1.controller.js
@@ -1,13 +1,22 @@
 sap.ui.define([
 	"sap/ui/core/mvc/Controller",
-	"sap/ui/model/json/JSONModel"
-], function (Controller) {
+	"sap/ui/model/json/JSONModel",
+	"zmodel/zmodel/controller/BaseController",
+	"sap/ui/core/UIComponent"
+], function (Controller,JSONModel,BaseController,UIComponent) {
 	"use strict";
 
-	return Controller.extend("zmodel.zmodel.controller.View1", {
+	return BaseController.extend("zmodel.zmodel.controller.View1", {
 		onInit: function () {
 			var employee = this.getOwnerComponent().getModel("newModel").getProperty("/employee");
 			var department = this.getOwnerComponent().getModel("newModel").getProperty("/department");
+		},
+		onPress:function(){
+			var employee = this .getOwnerComponent().getModel("newModel").getProperty("/employee");
+			var department = this.getOwnerComponent().getModel("newModel").getProperty("/department");
+			var b = 5;
+			var c = 10;
+			var a = this.randomCalculations(c,b);
 		}
 	});
 });
